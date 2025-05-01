fab054f841ce32aa7acd91ae81e1cbe72f12e8fd
danny <xi4869@gmail.com>
2025-05-01 14:43:49 +0800

    feat: 將密碼雜湊算法從 bcrypt 更改為 argon2，以增強安全性

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,11 @@
-const bcrypt = require("bcrypt");
+const argon2 = require("argon2");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
 exports.register = async (req, res) => {
 	try {
 		const { username, password } = req.body;
-		const hashedPassword = bcrypt.hashSync(password, 10);
+		const hashedPassword = await argon2.hash(password);
 		const user = await User.query().insert({
 			username,
 			password: hashedPassword,
@@ -23,7 +23,7 @@ exports.login = async (req, res) => {
 		if (!user) {
 			return res.status(401).json({ message: "用戶名或密碼錯誤" });
 		}
-		const isValidPassword = await bcrypt.compare(password, user.password);
+		const isValidPassword = await argon2.verify(user.password, password);
 		if (!isValidPassword) {
 			return res.status(401).json({ message: "用戶名或密碼錯誤" });
 		}
